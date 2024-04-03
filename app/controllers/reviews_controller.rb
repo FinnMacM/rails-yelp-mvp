fa288f0245106922c147d168757a7b0915ac54c7
Finn Mclaughlin <finnmacm@gmail.com>
2024-04-03 15:20:28 +0200

    pushing some styling as well as some error messages

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,19 +1,15 @@
 class ReviewsController < ApplicationController
+  before_action :set_restaurant, only: [:new, :create, :destroy]
+
   def new
-    @restaurants = Restaurant.find(params[:restaurant_id])
     @review = Review.new
   end
 
   def create
-    @review = Review.new(review_params)
-    @restaurants = Restaurant.find(params[:restaurant_id])
-    # @review.restaurant_id = @restaurant.id
-    # must belong to a restaurant in order to exist
-    @review.restaurant = @restaurants
-    # exclamaition mark shows errors if the instance DOESN'T save
+    @review = @restaurant.reviews.build(review_params)
 
-    if @review.save!
-      redirect_to restaurant_path(@restaurants)
+    if @review.save
+      redirect_to restaurant_path(@restaurant), notice: 'Review was successfully created.'
     else
       render :new, status: :unprocessable_entity
     end
@@ -21,13 +17,17 @@ class ReviewsController < ApplicationController
 
   def destroy
     @review = Review.find(params[:id])
-    @review.delete
-    redirect_to restaurant_path(@review.restaurant), status: :see_other
+    @review.destroy
+    redirect_to restaurant_path(@restaurant), status: :see_other, notice: 'Review was successfully deleted.'
   end
 
   private
 
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
   def review_params
-    params.require(:review).permit(:content, :rating)
+    params.require(:review).permit(:rating, :content)
   end
 end
